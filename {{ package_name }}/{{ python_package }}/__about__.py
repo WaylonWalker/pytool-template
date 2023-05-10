daced3babbf71f136867c13998fd23a5ee209bc8
Waylon S. Walker <waylon@waylonwalker.com>
2023-05-10 18:32:26 -0500

    init

--- {{ package_name }}/{{ python_package }}/__about__.py
+++ {{ package_name }}/{{ python_package }}/__about__.py
@@ -1,9 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Waylon S. Walker <waylon@waylonwalker.com>
 #
 # SPDX-License-Identifier: MIT
-import sys
-
-if __name__ == '__main__':
-    from .cli import coverage_rich
-
-    sys.exit(coverage_rich())
+__version__ = "0.0.0.dev1"
